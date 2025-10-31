0b532d76be7f6d96c022b5c9d412141449224ca8
Thu Thao <lethuthao071224@gmail.com>
2025-10-31 17:19:18 +0700

    fix subase

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,6 +1,12 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "Supabase URL or ANON KEY is missing. Please set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in your environment variables."
+  );
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
